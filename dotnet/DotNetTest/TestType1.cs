afcd7ff2e3c234b57c7d3a39567298491163f698
Steve Westbrook <StevenAWestbrook@gmail.com>
2017-02-15 08:33:45 -0500

    initial commit

---
+++ dotnet/DotNetTest/TestType1.cs
@@ -0,0 +1,17 @@
+using System;
+
+namespace DotNetTest
+{
+	public class TestType1
+	{
+		public TestType1()
+		{
+		}
+
+		public string Name { get; set; }
+
+		public TestType2 Child { get; private set; }
+
+	}
+}
+
---
+++ index.js
@@ -0,0 +1,12 @@
+/**
+ * index of edge-generator module
+ * Copyright(c) 2017 Steve Westbrook
+ * MIT Licensed
+ */
+
+'use strict';
+
+const EdgeReference = require('edge-reference.js');
+const ProxyGenerator = require('./src/proxy-generator.js');
+
+module.exports.ProxyGenerator = ProxyGenerator;
